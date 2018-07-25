5d072018272ff58e64f904faac3af7f34ba05877
georodent <deddedgood@gmail.com>
2018-07-25 06:38:18 -0700

    New files

---
+++ script.js
@@ -0,0 +1,21 @@
+// Infinite blink/fade 
+//function effectFadeIn(classname) {
+//$(".main-pic"+classname).fadeOut(8000).fadeIn(8000, effectFadeOut(classname))
+//}
+//function effectFadeOut(classname) {
+//$(".main-pic"+classname).fadeI8000).fadeOut(8000, effectFadeIn(classname))
+//}
+//$(document).ready(function(){
+//effectFadeIn('.main-pic')
+//   $(".main-pic").fadeout(500);
+//});
+//
+//
+//function blink(Infinity) {
+//$("a")
+//.fadeIn(50)
+//.fadeOut(100, function(){blink()});
+//}
+//$(function(){
+//blink();
+//});
